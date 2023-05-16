ddb3079914d90d10b1546ac72fa9b0c59f128698
Gabriel Schmitt Coelho <coelhosc08@gamil.com>
2023-05-16 19:17:40 -0300

    estudos

---
+++ javascript/type-number.js
@@ -0,0 +1,7 @@
+// tipo Number
+
+const meuNumero = 3;
+const n1 = 4;
+const n2 = 5;
+const operacaoMatematica = n1 + n2;
+console.log(operacaoMatematica)
---
+++ javascript/type-string.js
@@ -0,0 +1,9 @@
+const texto1 = "Olá, mundo!"
+const senha = "Senha123."
+const t1 = "barra velha"
+const t2 = "Barra Velha"
+const tique = "\u2705"
+const bvMinusculo = t2.toLowerCase
+
+
+console.log(t1.toLowerCase)
